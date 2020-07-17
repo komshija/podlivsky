8e762087dfef662f45b243468993bbac1f616b8b
agoskomsija <agostvv@gmail.com>
2020-07-17 21:49:49 +0200

    Updates

---
+++ precache-manifest.a93704115d27df4d1856e2478fe4b034.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d75fb70a9cbeb84508ab3fdc95666a",
+    "url": "/podlivsky/index.html"
+  },
+  {
+    "revision": "df6572ee5821d5e5e662",
+    "url": "/podlivsky/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ff0a80fdf1cb0de94590",
+    "url": "/podlivsky/static/css/main.6c843735.chunk.css"
+  },
+  {
+    "revision": "df6572ee5821d5e5e662",
+    "url": "/podlivsky/static/js/2.ed2fe8bb.chunk.js"
+  },
+  {
+    "revision": "6b83a8a8c872cd7011d9d896ee5f1e3d",
+    "url": "/podlivsky/static/js/2.ed2fe8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0a80fdf1cb0de94590",
+    "url": "/podlivsky/static/js/main.afce58fb.chunk.js"
+  },
+  {
+    "revision": "858b47b271e78b8de35a",
+    "url": "/podlivsky/static/js/runtime-main.594706a2.js"
+  },
+  {
+    "revision": "8ffc7c83d957f83e5e9f9b5db1e19ad7",
+    "url": "/podlivsky/static/media/basket.8ffc7c83.jpg"
+  },
+  {
+    "revision": "d019c53131442a0324adeae4ce96b679",
+    "url": "/podlivsky/static/media/brand-small.d019c531.png"
+  },
+  {
+    "revision": "05bf381a3aec720841d7304a829bc26c",
+    "url": "/podlivsky/static/media/bundesliga.05bf381a.jpg"
+  },
+  {
+    "revision": "0b1a77fa135cd3630c70917ba0f1b40c",
+    "url": "/podlivsky/static/media/ceovikend.0b1a77fa.jpg"
+  },
+  {
+    "revision": "c0124fce95932a19bcc3348af9235c2e",
+    "url": "/podlivsky/static/media/dodjihitno.c0124fce.jpg"
+  },
+  {
+    "revision": "f6b2d63083024525cab56cc7e79f6955",
+    "url": "/podlivsky/static/media/font.f6b2d630.otf"
+  },
+  {
+    "revision": "80e1375185018b2a78027083bd13e157",
+    "url": "/podlivsky/static/media/korakispred.80e13751.jpg"
+  },
+  {
+    "revision": "e69b06b0a438c59575d5058864444b40",
+    "url": "/podlivsky/static/media/logo-red.e69b06b0.png"
+  },
+  {
+    "revision": "1400d2d1a43ab00e8cd2c483694a7c6b",
+    "url": "/podlivsky/static/media/logo.1400d2d1.png"
+  },
+  {
+    "revision": "c80a1d0b838c2e7374180b22c4381103",
+    "url": "/podlivsky/static/media/podlagos.c80a1d0b.jpg"
+  },
+  {
+    "revision": "32fc642c6656b4428ec298568a60f3d1",
+    "url": "/podlivsky/static/media/thotpatrola.32fc642c.jpg"
+  },
+  {
+    "revision": "7ec57fedfa359ad4b29545b3e4ea6415",
+    "url": "/podlivsky/static/media/vinopijem.7ec57fed.jpg"
+  }
+]);
